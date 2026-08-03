e1c8dc29be137257855f7ade1dfa7fb2ee9588b2
Subash B <subash59245@gmail.com>
2026-08-03 22:32:20 +0530

    feat: added a login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,19 @@
 import { ThemeProvider } from "./context/ThemeContext";
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import useLenis from "./hooks/useLenis";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 function App() {
   useLenis();
   return (
     <ThemeProvider>
-      <Home />
+      <Router>
+        <Routes>
+          <Route element={<Home/>} path="/"/>
+          <Route element={<Login/>} path="/login"/>
+        </Routes>
+      </Router>
     </ThemeProvider>
   );
 }
